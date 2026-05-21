f7dc35ac6c91468e96d70c14b7cc622ea6db559e
usmankashif-dev <usmankashif711@gmail.comm>
2026-05-21 12:48:32 +0500

    Fix: Add settings namespace to all settings routes - prevent profile.edit/password.edit conflicts

--- routes/settings.php
+++ routes/settings.php
@@ -8,14 +8,14 @@ use Inertia\Inertia;
 Route::middleware('auth')->group(function () {
     Route::redirect('settings', '/settings/profile');
 
-    Route::get('settings/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('settings/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('settings/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('settings/profile', [ProfileController::class, 'edit'])->name('settings.profile.edit');
+    Route::patch('settings/profile', [ProfileController::class, 'update'])->name('settings.profile.update');
+    Route::delete('settings/profile', [ProfileController::class, 'destroy'])->name('settings.profile.destroy');
 
-    Route::get('settings/password', [PasswordController::class, 'edit'])->name('password.edit');
-    Route::put('settings/password', [PasswordController::class, 'update'])->name('password.update');
+    Route::get('settings/password', [PasswordController::class, 'edit'])->name('settings.password.edit');
+    Route::put('settings/password', [PasswordController::class, 'update'])->name('settings.password.update');
 
     Route::get('settings/appearance', function () {
         return Inertia::render('settings/Appearance');
-    })->name('appearance');
+    })->name('settings.appearance');
 });
